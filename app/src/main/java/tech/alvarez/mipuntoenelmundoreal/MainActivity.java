234e5fda785457ca7e211539051443e99930fcfc
Daniel Alvarez <daniel@alvarez.tech>
2020-05-25 17:11:56 -0400

    Update gradle, AndroidX, dependencies

--- app/src/main/java/tech/alvarez/mipuntoenelmundoreal/MainActivity.java
+++ app/src/main/java/tech/alvarez/mipuntoenelmundoreal/MainActivity.java
@@ -3,11 +3,12 @@ package tech.alvarez.mipuntoenelmundoreal;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -25,8 +26,8 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        latitudTextView = (TextView) findViewById(R.id.latitudTextView);
-        longitudTextView = (TextView) findViewById(R.id.longitudTextView);
+        latitudTextView = findViewById(R.id.latitudTextView);
+        longitudTextView = findViewById(R.id.longitudTextView);
 
         googleApiClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
